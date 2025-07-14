6988b43aeed541dbb5e1c5fad3aa35fbc12b2402
PastelDoCeu <pastelariapasteldoceu@gmail.com>
2025-07-13 23:21:29 -0300

    Add files via upload

--- assets/script/dinamica-cards.js
+++ assets/script/dinamica-cards.js
@@ -2,8 +2,11 @@
     import { produtos } from './bancodedados.js'
 
     var cardapio = document.querySelector('.cardapio')
+      var cardapio2 = document.querySelector('.cardapio2')
+        var cardapio3 = document.querySelector('.cardapio3')
 
     function listarProdutos(){ produtos.forEach( produto => {
+
         if(produto.tipo == 'salgado'){
             cardapio.innerHTML += `
             <div class="card">
@@ -25,9 +28,52 @@
                         </div>
                     </div>
                 </div>`
+        }else if(produto.tipo == 'doce'){
+            cardapio2.innerHTML += `
+            <div class="card">
+                <img src="#" alt="">
+                <div class="produto">
+                    <span class="nome">${produto.nome}</span>
+                    <span class="descricao">${produto.descricao}</span>
+                    <div class="acoes">
+                        <div class="quantidade">
+                            <input type="button" value="-" class="menos" onclick="remover(${produto.id})">
+                            <div class="contador" id="${produto.id}">${produto.quantia}</div>
+                            <input type="button" value="+" class="mais" onclick="adicionar(${produto.id})">
+                        </div>
+                        <input type="button" value="Adicionar" class="adicionar" >
+                    </div>
+                        <div class="preco">
+                            <span class="promocao"></span>
+                            <span class="valor">R$ ${produto.preco.toFixed(2)}</span>
+                        </div>
+                    </div>
+                </div>`
         }
-    })
-}
+         if(produto.tipo == 'bebida'){
+            cardapio3.innerHTML += `
+            <div class="card">
+                <img src="#" alt="">
+                <div class="produto">
+                    <span class="nome">${produto.nome}</span>
+                    <span class="descricao">${produto.descricao}</span>
+                    <div class="acoes">
+                        <div class="quantidade">
+                            <input type="button" value="-" class="menos" onclick="remover(${produto.id})">
+                            <div class="contador" id="${produto.id}">${produto.quantia}</div>
+                            <input type="button" value="+" class="mais" onclick="adicionar(${produto.id})">
+                        </div>
+                        <input type="button" value="Adicionar" class="adicionar" >
+                    </div>
+                        <div class="preco">
+                            <span class="promocao"></span>
+                            <span class="valor">R$ ${produto.preco.toFixed(2)}</span>
+                        </div>
+                    </div>
+                </div>`
+        } 
+    } )  }
+
 
     window.adicionar = function adicionar(id){
         
@@ -42,8 +88,24 @@
         
     }
 
-
     
-  
+    listarProdutos();
 
-listarProdutos();
+    document.querySelectorAll('input[name="menu-opcoes"]').forEach((input) => {
+    input.addEventListener('change', (e) => {
+        // Esconde todos os cardápios
+        document.querySelectorAll('.cardapio, .cardapio2, .cardapio3').forEach((c) => {
+            c.style.opacity = 0;
+            c.style.visibility = 'hidden';
+        });
+        console.log(e.target.value)
+        // Mostra o cardápio correspondente ao valor
+        const cardapioMostrar = document.getElementById(e.target.value);
+        if (cardapioMostrar) {
+            cardapioMostrar.style.opacity = 1;
+            cardapioMostrar.style.visibility = 'visible';
+        } else {
+            console.log("Cardápio não encontrado:", e.target.value);
+        }
+    });
+});
